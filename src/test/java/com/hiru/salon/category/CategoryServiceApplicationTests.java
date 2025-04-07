2eb8b35a96b2444d41b780673807cd800543d758
itz-Hiru <hirumithagithub@gmail.com>
2025-04-07 09:12:45 +0530

    Created simple springboot project

---
+++ src/test/java/com/hiru/salon/category/CategoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiru.salon.category;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
